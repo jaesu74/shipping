bdec1bae517b2d9bcdfb382792b26bfd9c01c340
jaesu <jaesu@kakao.com>
2025-05-01 20:32:15 +0900

    테스트 환경 설정 추가: Babel, Jest 설정 및 의존성 추가

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+}; 
